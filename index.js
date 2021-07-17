dce7fccb73f7aaa296f471407e8d50dd1039d43c
misobarisic <miki.barisic5@gmail.com>
2021-07-17 20:14:40 +0200

    Just to be safe

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const {downloadQuery, downloadUser} = require("./lib/")
 downloadQuery("ass", __dirname, 1)
+    .on("start",() => {
+        console.log("onStart")
+    })
+    .on("finish",(name,size,filePath) => {
+    console.log("onFinish",{name,size,filePath})
+})
+
+
+
+// downloadUser("annamonik",__dirname,1)
