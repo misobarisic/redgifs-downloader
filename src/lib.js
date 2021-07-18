61f8bde8b5c1a69025ef269ee7c4a7e6e6bc18c7
misobarisic <miki.barisic5@gmail.com>
2021-07-18 13:07:13 +0200

    Small change

--- src/lib.js
+++ src/lib.js
@@ -1,6 +1,8 @@
+const axios = require("axios")
+const fs = require("fs")
+const path = require("path")
 const EventEmitter = require('events')
 const {getLinksMain,filter} = require("./links")
-const { isBrowser, isNode} = require("browser-or-node")
 
 module.exports = {
     create,
@@ -31,80 +33,73 @@ function create(dirname) {return new Downloader(dirname)}
 function instance(dirname) {return new Downloader(dirname)}
 
 async function main(downloader, userMode, query, options) {
-    // Can only be run inside node
-    if (isNode) {
-        const axios = require("axios")
-        const fs = require("fs")
-        const path = require("path")
+    const {dirname, eventEmitter} = downloader
+    const {minDuration, maxDuration, minLikes, minViews, numberToDownload, nsfw} = options
 
-        const {dirname, eventEmitter} = downloader
-        const {minDuration, maxDuration, minLikes, minViews, numberToDownload,nsfw} = options
+    if (minDuration && maxDuration) throw(new Error("Make sure minDuration and maxDuration aren't both defined"))
 
-        if (minDuration && maxDuration) throw(new Error("Make sure minDuration and maxDuration aren't both defined"))
+    eventEmitter.emit("onInit", {userMode, query, ...options, date: new Date()})
 
-        eventEmitter.emit("onInit", {userMode, query, ...options, date: new Date()})
-
-        async function download(gfycats, index = 0) {
-            try {
-                const length = numberToDownload || gfycats.length
-                if (index !== length) {
-                    const gfycat = gfycats[index]
-                    const {gfyName: name, mp4Url, views, likes, dislikes, userName: user} = gfycat
-                    const size = gfycat.content_urls.mp4.size
-                    const finalPath = path.join(dirname, `/${query || "trending"}/${name}.mp4`)
-                    const meta = {dislikes, likes, name, size, user, url: mp4Url, views}
-
-                    const writer = fs.createWriteStream(finalPath)
-                    writer.on("close", () => {
-                        eventEmitter.emit("onFileDownloadFinish", {...meta, date: new Date()})
-                        download(gfycats, index + 1)
-                    })
-
-                    eventEmitter.emit("onFileDownloadStart", {...meta, date: new Date()})
-                    axios.get(mp4Url, {responseType: "stream"})
-                        .then(async response => {
-                            response.data.pipe(writer)
-                        })
-                } else {
-                    eventEmitter.emit("onFinish", {
-                        availableFiles: gfycats.length,
-                        date: new Date(),
-                        numberToDownload: numberToDownload,
-                        query,
-                        userMode
-                    })
-                }
-            } catch (error) {
-                eventEmitter.emit("onError", {downloader, error})
-            }
-        }
-
-        let gfycats = []
+    async function download(gfycats, index = 0) {
         try {
-            await getLinksMain(gfycats,userMode,query,options)
+            const length = numberToDownload || gfycats.length
+            if (index !== length) {
+                const gfycat = gfycats[index]
+                const {gfyName: name, mp4Url, views, likes, dislikes, userName: user} = gfycat
+                const size = gfycat.content_urls.mp4.size
+                const finalPath = path.join(dirname, `/${query || "trending"}/${name}.mp4`)
+                const meta = {dislikes, likes, name, size, user, url: mp4Url, views}
 
-            try {
-                fs.mkdirSync(path.join(dirname, `/${query || "trending"}`))
-            } catch (e) {
+                const writer = fs.createWriteStream(finalPath)
+                writer.on("close", () => {
+                    eventEmitter.emit("onFileDownloadFinish", {...meta, date: new Date()})
+                    download(gfycats, index + 1)
+                })
+
+                eventEmitter.emit("onFileDownloadStart", {...meta, date: new Date()})
+                axios.get(mp4Url, {responseType: "stream"})
+                    .then(async response => {
+                        response.data.pipe(writer)
+                    })
+            } else {
+                eventEmitter.emit("onFinish", {
+                    availableFiles: gfycats.length,
+                    date: new Date(),
+                    numberToDownload: numberToDownload,
+                    query,
+                    userMode
+                })
             }
-
-            eventEmitter.emit("onStart", {
-                availableFiles: gfycats.length,
-                date: new Date(),
-                numberToDownload: numberToDownload,
-                query,
-                userMode
-            })
-
-            gfycats = filter(gfycats,options)
-            eventEmitter.emit("onGetLinks", {
-                links: gfycats,
-                date: new Date(),
-                options
-            })
-            download(gfycats)
-        } catch (e) {
-            console.log("Something went wrong", e)
+        } catch (error) {
+            eventEmitter.emit("onError", {downloader, error})
         }
+    }
+
+    let gfycats = []
+    try {
+        await getLinksMain(gfycats, userMode, query, options)
+
+        try {
+            fs.mkdirSync(path.join(dirname, `/${query || "trending"}`))
+        } catch (e) {
+        }
+
+        eventEmitter.emit("onStart", {
+            availableFiles: gfycats.length,
+            date: new Date(),
+            numberToDownload: numberToDownload,
+            query,
+            userMode
+        })
+
+        gfycats = filter(gfycats, options)
+        eventEmitter.emit("onGetLinks", {
+            links: gfycats,
+            date: new Date(),
+            options
+        })
+        download(gfycats)
+    } catch (e) {
+        console.log("Something went wrong", e)
     }
 }
