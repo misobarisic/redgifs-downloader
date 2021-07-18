ee324fd548dc8028225d22063c9c51f8b3460687
misobarisic <miki.barisic5@gmail.com>
2021-07-18 08:27:22 +0200

    beta@1.0.0

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,8 @@ import json from 'rollup-plugin-json';
 import replace from 'rollup-plugin-re';
 import resolve from 'rollup-plugin-node-resolve';
 import strip from 'rollup-plugin-strip';
-import unlazy from './unlazy.plugin.js';
 import visualizer from 'rollup-plugin-visualizer';
+import {terser} from 'rollup-plugin-terser';
 
 const sourceMap = true;
 
@@ -50,6 +50,40 @@ const config = [{
     commonjs({sourceMap}),
     json()
   ]
-}];
+},
+  {
+    input: './src/',
+    output: [{
+      file: './dist/lib.js',
+      format: 'cjs',
+      banner: '#!/usr/bin/env node',
+      sourcemap: sourceMap
+    },
+      {
+        file: './dist/lib.min.js',
+        format: 'iife',
+        sourcemap: sourceMap,
+        plugins: [terser()]
+      }],
+    external: require('module').builtinModules,
+    plugins: [
+      replace({
+        patterns: [{
+          test: /require\('debug'\)/gm,
+          replace: '(() => () => {})'
+        }]
+      }),
+      strip({functions: ['debug'], sourceMap}),
+      resolve(),
+      commonjs({sourceMap}),
+      json(),
+      copy({
+        files: require.resolve('./xdg-open'),
+        dest: __dirname,
+        options: {verbose: true}
+      }),
+      visualizer()
+    ]
+  }];
 
 export default config;
